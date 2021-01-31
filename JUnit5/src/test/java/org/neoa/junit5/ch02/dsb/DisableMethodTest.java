b8b4f499b50029c6cffb1a20521219b28b652f8a
Cristian Flores Martínez <acrixf@gmail.com>
2021-01-31 16:29:02 -0600

    Disable Method Level

---
+++ JUnit5/src/test/java/org/neoa/junit5/ch02/dsb/DisableMethodTest.java
@@ -0,0 +1,27 @@
+package org.neoa.junit5.ch02.dsb;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.neoa.junit5.ch02.lfcm.SUT;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DisableMethodTest {
+
+    private SUT systemUnderTest = new SUT("Our System under test");
+
+    @Test
+    @Disabled
+    void testRegularWork() {
+        boolean canReceiveRegularWork = systemUnderTest.canReceiveRegularWork();
+        assertTrue(canReceiveRegularWork);
+    }
+
+    @Test
+    @Disabled("Feature still under construction")
+    void testAdditionalWork() {
+        boolean canReceiveAdditionalWork = systemUnderTest.canReceiveAdditionalWork();
+        assertFalse(canReceiveAdditionalWork);
+    }
+}
