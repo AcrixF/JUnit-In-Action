5b6d3b47bfbfb0ae8b92d29da535796e50c561a5
Cristian Flores Martínez <acrixf@gmail.com>
2021-01-31 10:50:40 -0600

    update git ignore file - add track in test class

--- JUnit5/src/main/java/org/neoa/junit5/ch01/CalculatorTest.java
+++ JUnit5/src/main/java/org/neoa/junit5/ch01/CalculatorTest.java
@@ -2,7 +2,23 @@ package org.neoa.junit5.ch01;
 
 public class CalculatorTest {
 
+    private int nbErrors = 0;
+
+
     public static void main(String[] args) {
+        CalculatorTest test = new CalculatorTest();
+        try {
+            test.testAdd();
+        } catch (Throwable e) {
+            test.nbErrors++;
+            e.printStackTrace();
+        }
+        if (test.nbErrors > 0) {
+            throw  new IllegalArgumentException("There were " + test.nbErrors + "error(s)");
+        }
+    }
+
+    private static void testAdd() {
         Calculator calculator = new Calculator();
         double result = calculator.add(10, 50);
         if (result != 60) {
