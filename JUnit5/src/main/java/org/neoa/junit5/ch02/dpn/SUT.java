8e89f5778f2676fc7711c251129b49972d547087
Cristian Flores Martínez <acrixf@gmail.com>
2021-01-31 15:21:30 -0600

    Move local variable to Constant level

--- JUnit5/src/main/java/org/neoa/junit5/ch02/dpn/SUT.java
+++ JUnit5/src/main/java/org/neoa/junit5/ch02/dpn/SUT.java
@@ -2,15 +2,19 @@ package org.neoa.junit5.ch02.dpn;
 
 public class SUT {
 
+    public static final String HELLO = "Hello";
+    public static final String BYE = "Bye";
+    public static final String HOW_ARE_YOU = "How are you?";
+
     String hello() {
-        return "Hello";
+        return HELLO;
     }
 
     String bye() {
-        return "Bye";
+        return BYE;
     }
 
     String talk() {
-        return "How are you?";
+        return HOW_ARE_YOU;
     }
 }
