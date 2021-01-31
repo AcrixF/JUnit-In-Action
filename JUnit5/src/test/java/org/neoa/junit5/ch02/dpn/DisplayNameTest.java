3a61202af3fea2b5e8e5b4f2af7676182bf0d6b6
Cristian Flores Martínez <acrixf@gmail.com>
2021-01-31 15:19:00 -0600

    Using @DisplayName Annotation
    @DisplayName annotations can work at:
     - Class Level
     - Method Level 
    
    If a method does not has a @DisplayName it will show as identifier the method's name.

---
+++ JUnit5/src/test/java/org/neoa/junit5/ch02/dpn/DisplayNameTest.java
@@ -0,0 +1,29 @@
+package org.neoa.junit5.ch02.dpn;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DisplayName("Test class showing the @DisplayName annotation")
+public class DisplayNameTest {
+
+    private SUT systemUnderTest = new SUT();
+
+    @Test
+    @DisplayName("Our system under test says hello.")
+    void testHello() {
+        assertEquals("Hello", systemUnderTest.hello());
+    }
+
+    @Test
+    @DisplayName("\uD83D\uDE06")
+    void testTalking() {
+        assertEquals("How are you?", systemUnderTest.talk());
+    }
+
+    @Test
+    void testBye() {
+        assertEquals("Bye", systemUnderTest.bye());
+    }
+}
