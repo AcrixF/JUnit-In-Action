a67e05e910aa06a1c20fe3d79b36188078337275
Cristian Flores Martínez <acrixf@gmail.com>
2021-01-31 15:17:22 -0600

    Class to test

---
+++ JUnit5/src/main/java/org/neoa/junit5/ch02/dpn/SUT.java
@@ -0,0 +1,16 @@
+package org.neoa.junit5.ch02.dpn;
+
+public class SUT {
+
+    String hello() {
+        return "Hello";
+    }
+
+    String bye() {
+        return "Bye";
+    }
+
+    String talk() {
+        return "How are you?";
+    }
+}
