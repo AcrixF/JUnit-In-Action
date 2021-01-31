796b07c5dc652eb13fb88212a009bd20d3484b2a
Cristian Flores Martínez <acrixf@gmail.com>
2021-01-31 15:00:24 -0600

    Life Cycle Methods

--- JUnit5/src/test/java/org/neoa/junit5/ch02/lfcm/SUTTest.java
+++ JUnit5/src/test/java/org/neoa/junit5/ch02/lfcm/SUTTest.java
@@ -1,2 +1,50 @@
-package org.neoa.junit5.ch02.lfcm;public class SUTTest {
+package org.neoa.junit5.ch02.lfcm;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SUTTest {
+
+    private static ResourceForAllTests resourceForAllTests;
+
+    private SUT systemUnderTest;
+
+    @BeforeAll
+    static void setUpClass() {
+        resourceForAllTests = new ResourceForAllTests("Our resource for all tests.");
+    }
+
+    @AfterAll
+    static void tearDownClass() {
+        resourceForAllTests.close();
+    }
+
+    @BeforeEach
+    void setUp() {
+        systemUnderTest = new SUT("Our System, under test");
+    }
+
+    @AfterEach
+    void testDown() {
+        systemUnderTest.close();
+    }
+
+    @Test
+    void testRegularWork() {
+        boolean canReceiveRegularWork = systemUnderTest.canReceiveRegularWork();
+        assertTrue(canReceiveRegularWork);
+    }
+
+    @Test
+    void testAdditionalWork() {
+        boolean canReceiveAdditionalWork = systemUnderTest.canReceiveAdditionalWork();
+        assertFalse(canReceiveAdditionalWork);
+    }
+
 }
